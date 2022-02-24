39d690068bd47fc10b99e72e2c9a5b963942ccb9
unknown <volkov95@inbox.ru>
2022-02-24 16:29:08 +0500

    many changes

---
+++ crud.php
@@ -0,0 +1,130 @@
+<?php // Добавлять сообщения обо всех ошибках, кроме E_NOTICE
+error_reporting(E_ALL & ~E_NOTICE); ?>
+<?php
+session_start(); ?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>CRUD</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" idegrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">test</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?php echo 'index.php'; ?>">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo 'index.php'; ?>">Logout</a>
+                </li>
+        </div>
+    </nav>
+
+    <?php
+    require_once 'config.php';
+    ?>
+
+    <?php
+    if (isset($_SESSION['message'])) :
+    ?>
+        <div class="alert alert-<?= $_SESSION['msg_type'] ?>">
+            <?php
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+            ?>
+        </div>
+
+    <?php endif ?>
+
+    <div class="container">
+        <?php
+        // show table 
+        $mysqli = new mysqli('localhost', 'root', '', 'dbcrud') or die(mysqli_error($mysqli));
+        $result = $mysqli->query("SELECT * FROM data") or die($mysqli->error);      // error with the database 
+        // check the db
+        ?>
+        <div class="row justify-content-center">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last name</th>
+                        <th>Image</th>
+                        <th>Created at</th>
+                        <th colspan="2">Action</th>
+                    </tr>
+                </thead>
+                <?php
+                while ($row = $result->fetch_assoc()) :
+                ?>
+                    <tr>
+                        <td><?php echo $row['fname']; ?></td>
+                        <td><?php echo $row['lname']; ?></td>
+                        <td><?php echo $row['file_name']; ?></td>
+                        <td><?php echo $row['created_at']; ?></td>
+                        <td>
+                            <a href="crud.php?edit=<?php echo $row['id']; ?>" class="btn btn-info">Edit</a>
+                            <a href="crud.php?delete=<?php echo $row['id']; ?>" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                <?php endwhile; ?>
+            </table>
+        </div>
+
+        <div class="row justify-content-center">
+            <form action="config.php" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <?php if ($id) : ?>
+                        <input type="text" name="name" value="<?php echo $id; ?>">
+                    <?php endif; ?>
+                    <div class="form-group">
+                        <label for="">First Name: </label>
+                        <input type="text" name="fname" class="from-control" value="<?php echo $fname;  ?>" placeholder="Enter your first name">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Last Name: </label>
+                        <input type="text" name="lname" class="from-control" value="<?php echo $lname; ?>" placeholder="Enter your last name">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Image: </label>
+                        <input type="file" name="imge" class="from-control" value="">
+                        <span class="error"><?php echo $errmsg ?></span>
+                    </div>
+                    <div class="form-group">
+                        <?php
+                        if ($update == true) :
+                        ?>
+                            <button type="submit" class="btn btn-info" name="update">Update</button>
+                        <?php
+                        else :
+                        ?>
+                            <button type="submit" class="btn btn-primary" name="save">SAVE</button>
+                        <?php
+                        endif;
+                        ?>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" idegrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" idegrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" idegrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
