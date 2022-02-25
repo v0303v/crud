71f2b2e7e44752669a4beffadd188f2590cb8f70
unknown <volkov95@inbox.ru>
2022-02-25 11:31:16 +0500

    не душни пепега

---
+++ Authentication/users.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Фев 25 2022 г., 09:27
+-- Версия сервера: 10.3.29-MariaDB-log
+-- Версия PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `dbarf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`) VALUES
+(1, '123', '123'),
+(12, '999', '999'),
+(13, '555', '555'),
+(15, '333', '333'),
+(84, 'sdfsfsd', 'sdfjsduikgh'),
+(85, 's5456', 'sdfj456ikgh'),
+(143, 'sdkfjk', 'sfkdf'),
+(145, 'sdfsd', 'sdfds'),
+(148, '1', '1'),
+(153, '44564', '45456456'),
+(160, 'op[op[', 'op[op[o'),
+(177, 'pepepep', 'pepepepep'),
+(180, '5000', '5000'),
+(182, '4545', '4545'),
+(184, '546145343', '4563456456'),
+(188, '214', '[pp'),
+(193, '214', 'wewe'),
+(200, 'ytutuyty', 'tyututyu');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=202;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
